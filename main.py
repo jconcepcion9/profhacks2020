1d52c959973d33e23ec362efad96ce553b1f4560
dylan <dylanpaulmcrae@gmail.com>
2020-02-29 15:31:12 -0500

    added a label in the GUI

--- main.py
+++ main.py
@@ -2,7 +2,13 @@
 import lib
 import tkinter
 
+#create a window
 top = tkinter.Tk()
+#create a label in the 'top' window
+w = tkinter.Label(top, text="Hello, world!")
+#?
+w.pack()
+#run the window. this line will not finish until the window closes
 top.mainloop()
 
 lib.hello("A team")
