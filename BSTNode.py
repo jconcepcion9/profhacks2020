ab655bcbd31be9e06d0a3775c3d82769b244346b
dylan <dylanpaulmcrae@gmail.com>
2020-02-29 15:15:04 -0500

    added python BSTNode

--- BSTNode.py
+++ BSTNode.py
@@ -1,1 +1,31 @@
-class BSTNode
+class BSTNode:
+    '''
+    properties
+        info  : info stored in current node. must be comparable
+        left  : left node
+        right : right node
+    '''
+
+    def __init__(self, info):
+        self.info = info
+        left = None
+        right = None
+
+    def setInfo(self, info):
+        self.info = info
+    
+    def getInfo(self):
+        return self.info
+ 
+    def setLeft(self, link):
+        self.left = link
+
+    def setRight(self, link):
+        self.right = link
+
+    def getLeft(self):
+        return self.left
+
+
+    def getRight(self):
+        return self.right
