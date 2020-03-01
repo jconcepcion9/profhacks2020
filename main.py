bb67fc492899dea1636fc1d98b0c0b3769569068
dylan <dylanpaulmcrae@gmail.com>
2020-03-01 08:02:33 -0500

    implimented player turns

--- main.py
+++ main.py
@@ -8,10 +8,31 @@ import threading
 db = debugwindow.DebugWindow()
 threading.Thread(target=db.run, args=(1,))
 
+
+# set up window
+root = tkinter.Tk()
+root.geometry("900x900")
+top_label = tkinter.Label(root, text="your turn", foreground="blue") # the label for the current player
+top_label.pack()
+game_window = ui.GameWindow(root)
+
+# set the top label to change then the turn changes
+def turn_change(is_player_turn:bool):
+    if is_player_turn:
+        top_label.config(text="your turn", foreground="blue")
+    else:
+        top_label.config(text="opponent turn", foreground="red")
+
+game_window.set_switch_turn(turn_change)
+
 #function run when a cell is clicked
-def onclick(a, b):
-    db.set_other("you clicked cell "+str(a)+" "+str(b))
+def onclick(x:int, y:int, game_window:debugwindow.DebugWindow):
+    db.set_other("you clicked cell "+str(x)+" "+str(y))
+    game_window.disable_buttons()
+    game_window.opponent_turn()
+    turn_change(game_window.is_player_turn)
+
+game_window.set_on_click(onclick)
 
 #start the main game window
-game_window = ui.GameWindow(onclick)
-game_window.run()
+root.mainloop()
